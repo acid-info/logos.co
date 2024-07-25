84333abb60c730480fad39809fb0e33aeaf0d28e
markoburcul <marko@status.im>
2024-07-25 10:32:11 +0200

    Resolving PR comments: added nix formatter, pinned nodejs version to 20, executed formatter on flake.nix.

--- flake.nix
+++ flake.nix
@@ -5,21 +5,28 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-        supportedSystems = ["x86_64-linux"  "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-        forEach = nixpkgs.lib.genAttrs supportedSystems;
-        pkgsFor = nixpkgs.lib.genAttrs supportedSystems (
-            system: import nixpkgs { inherit system; }
-        );
-      in rec {
-        devShells = forEach (system: {
-            default = pkgsFor.${system}.mkShellNoCC {
-                packages = with pkgsFor.${system}.buildPackages; [
-                    yarn # 1.22.22 
-                    nodejs # v20.15.1
-                ];
-            };
-        });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = forEachSystem (system: import nixpkgs { inherit system; });
+    in
+    rec {
+      formatter = forEachSystem (system: pkgsFor.${system}.nixpkgs-fmt);
+
+      devShells = forEachSystem (system: {
+        default = pkgsFor.${system}.mkShellNoCC {
+          packages = with pkgsFor.${system}.buildPackages; [
+            yarn # 1.22.22
+            nodejs_20 # v20.15.1
+          ];
+        };
+      });
     };
 }

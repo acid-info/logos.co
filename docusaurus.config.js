5817bb8b69f27e21318f8fba7a0b0bde9ba2adf9
jinhojang6 <jinho5051@gmail.com>
2024-09-25 23:37:44 +0900

    chore: add psc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,6 +105,10 @@ const config = {
                 href: '/privacy-policy',
                 label: 'Privacy Policy',
               },
+              {
+                href: '/security',
+                label: 'Security',
+              },
             ],
           },
         ],

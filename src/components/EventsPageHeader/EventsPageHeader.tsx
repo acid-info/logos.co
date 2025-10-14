8cc886f9e3994fe9e29e5c30df64e7eb921ba250
JulesFILIOT <dev@julesfiliot.com>
2025-10-14 11:22:11 +0900

    fix(events-page): using md syntax on jsx file

--- src/components/EventsPageHeader/EventsPageHeader.tsx
+++ src/components/EventsPageHeader/EventsPageHeader.tsx
@@ -1,41 +1,27 @@
 import React from 'react'
 import './EventsPageHeader.css'
-// import { Button } from '@acid-info/lsd-react'
+import Link from '@docusaurus/Link'
 
 const EventsPageHeader = () => {
-  // const scrollToSection = (sectionId: string) => {
-  //   const element = document.getElementById(sectionId)
-  //   if (element) {
-  //     element.scrollIntoView({ behavior: 'smooth' })
-  //   }
-  // }
-
   return (
     <div className="events-page-header">
       <div className="description">
-        Logos Circles are self-organised groups that take action to solve 
-        issues that matter locally. They are a form of parallel governance: 
-        instead of reforming broken or rigged institutions, they build better 
-        ones that live on or are sustained by the network.
+        Logos Circles are self-organised groups that take action to solve issues
+        that matter locally. They are a form of parallel governance: instead of
+        reforming broken or rigged institutions, they build better ones that
+        live on or are sustained by the network.
       </div>
-      Find a Circle near you to join the movement. Don’t see a Circle near you? 
-      [Join the server](https://discord.com/invite/logosnetwork) and share a proposal to start your own.
-      {/* <div className="actions">
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => scrollToSection('upcoming-events')}
-        >
-          Upcoming
-        </Button>
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => scrollToSection('past-events')}
-        >
-          Past
-        </Button>
-      </div> */}
+      <div>
+        Find a Circle near you to join the movement. Don’t see a Circle near
+        you?
+        <br />
+        <span>
+          <Link href="https://discord.com/invite/logosnetwork" target="_blank">
+            Join the server
+          </Link>
+          {' and share a proposal to start your own.'}
+        </span>
+      </div>
     </div>
   )
 }
